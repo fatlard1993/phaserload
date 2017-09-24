76aaefb803409acec13dd46672e4ef7ba24f2739
fatlard1993 <fatlard1993@gmail.com>
2017-09-24 14:50:36 -0700

    fix path for when woeking with homeserver

--- src/js/states/load.js
+++ src/js/states/load.js
@@ -17,16 +17,16 @@ Game.states.load.prototype.preload = function(){
 
   this.game.load.setPreloadSprite(preloadFg);
 
-  this.game.load.spritesheet('hud', 'assets/hud.png', 320, 256);
-  this.game.load.spritesheet('ground', 'assets/ground.png', 64, 64, 4);
-  this.game.load.spritesheet('ground_blue', 'assets/ground_blue.png', 64, 64, 4);
-  this.game.load.spritesheet('ground_green', 'assets/ground_green.png', 64, 64, 4);
-  this.game.load.spritesheet('ground_purple', 'assets/ground_purple.png', 64, 64, 4);
-  this.game.load.spritesheet('ground_teal', 'assets/ground_teal.png', 64, 64, 4);
-  this.game.load.spritesheet('ground_red', 'assets/ground_red.png', 64, 64, 4);
-  this.game.load.spritesheet('drill', 'assets/drill.png', 64, 64);
-  this.game.load.spritesheet('lava', 'assets/lava.png', 64, 64);
-  this.game.load.spritesheet('monster', 'assets/monster.png', 64, 64);
+  this.game.load.spritesheet('hud', '/assets/hud.png', 320, 256);
+  this.game.load.spritesheet('ground', '/assets/ground.png', 64, 64, 4);
+  this.game.load.spritesheet('ground_blue', '/assets/ground_blue.png', 64, 64, 4);
+  this.game.load.spritesheet('ground_green', '/assets/ground_green.png', 64, 64, 4);
+  this.game.load.spritesheet('ground_purple', '/assets/ground_purple.png', 64, 64, 4);
+  this.game.load.spritesheet('ground_teal', '/assets/ground_teal.png', 64, 64, 4);
+  this.game.load.spritesheet('ground_red', '/assets/ground_red.png', 64, 64, 4);
+  this.game.load.spritesheet('drill', '/assets/drill.png', 64, 64);
+  this.game.load.spritesheet('lava', '/assets/lava.png', 64, 64);
+  this.game.load.spritesheet('monster', '/assets/monster.png', 64, 64);
 };
 
 Game.states.load.prototype.create = function(){
