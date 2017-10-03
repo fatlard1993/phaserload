ba9da536aa39cbd2d4fcc6e12fea3b8a33c57d92
fatlard1993 <fatlard1993@gmail.com>
2017-10-02 18:42:26 -0700

    better welcome/greeting screen

--- src/js/states/lobby.js
+++ src/js/states/lobby.js
@@ -21,17 +21,17 @@ Game.states.lobby.prototype.addTitles = function(){
   var delay = 400;
   var tweenTime = 750;
 
-  var title = this.game.add.text(0, -100, 'Phaserload', { font: '48px '+ Game.config.font, fill: Game.config.textColor });
+  var title = this.game.add.text(0, 0, 'Phaserload', { font: '48px '+ Game.config.font, fill: Game.config.textColor });
   title.x = this.game.width/2 - title.getBounds().width/2;
   title.alpha = 0;
 
-  var text = this.game.add.text(0, this.game.height, 'Click or press [enter]', { font: '40px '+ Game.config.font, fill: Game.config.textColor });
+  var text = this.game.add.text(0, this.game.height, '[enter] or tap to begin', { font: '40px '+ Game.config.font, fill: Game.config.textColor });
   text.x = this.game.width/2 - text.getBounds().width/2;
   text.alpha = 0;
 
-  this.game.add.tween(title).to({ y: 100 }, tweenTime, Phaser.Easing.Elastic.Out, true, delay);
-  this.game.add.tween(title).to({ alpha: 1 }, tweenTime, Phaser.Easing.Sinusoidal.InOut, true, delay);
+  this.game.add.tween(title).to({ y: ((this.game.height / 4) * 2) - 148 }, tweenTime, Phaser.Easing.Elastic.Out, true, delay);
+  this.game.add.tween(title).to({ alpha: 1 }, tweenTime - 100, Phaser.Easing.Sinusoidal.InOut, true, delay);
 
-  this.game.add.tween(text).to({ y: 300 }, tweenTime, Phaser.Easing.Elastic.Out, true, delay + 150);
-  this.game.add.tween(text).to({ alpha: 1 }, tweenTime, Phaser.Easing.Sinusoidal.InOut, true, delay + 150);
+  this.game.add.tween(text).to({ y: (this.game.height / 4) * 2 }, tweenTime, Phaser.Easing.Elastic.Out, true, delay + 200);
+  this.game.add.tween(text).to({ alpha: 1 }, tweenTime - 100, Phaser.Easing.Sinusoidal.InOut, true, delay + 200);
 };
