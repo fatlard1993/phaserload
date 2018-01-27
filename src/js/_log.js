50a047c60518d19c8f9a96a51c1ddbc42cc0ca1f
fatlard1993 <fatlard1993@gmail.com>
2018-01-26 21:33:41 -0800

    add server support and cleanup, everything

--- src/js/_log.js
+++ src/js/_log.js
@@ -1,24 +1,55 @@
-/* global console, Dom */
+/* global Dom */
 
-var DBG = 0;
+var Log, LogHelp = {
+	colorMap: {
+		reset: '\x1b[0m',
+		info: '\x1b[34m',
+		warn: '\x1b[33m',
+		error: '\x1b[31m'
+	}
+};
 
-if(Dom.location.query.get('DBG')) DBG = parseInt(Dom.location.query.get('DBG'));
+LogHelp._noop = function _noop(){};
 
-function ConsoleWrap(method){
-  return function _log(verbosity){
-    if((verbosity || 0) < DBG && console && console[method]){
-      return console[method].bind(console);
-    }
-    else return function _noop(){};
-  };
+LogHelp._consoleWrap = function _consoleWrap(method){
+	return function _log(verbosity){
+		if((verbosity || 0) < LogHelp.DBG && console && console[method]){
+			if(LogHelp.isNode){
+				return function _colorLog(){
+					if(LogHelp.mapColors && LogHelp.colorMap[method]){
+						Array.prototype.unshift.call(arguments, LogHelp.colorMap[method]);
+						Array.prototype.push.call(arguments, LogHelp.colorMap.reset);
+					}
+					else if(method === 'error') Array.prototype.unshift.call(arguments, method);
+
+					console[method].apply(null, arguments);
+				};
+			}
+			return console[method].bind(console);
+		}
+
+		else return LogHelp._noop;
+	};
+};
+
+if(typeof Proxy === 'function') Log = new Proxy(LogHelp._consoleWrap('log'), { get(target, method){ return LogHelp._consoleWrap(method); } });
+
+else{
+	Log = LogHelp._consoleWrap('log');
+	Log.warn = LogHelp._consoleWrap('warn');
+	Log.error = LogHelp._consoleWrap('error');
+
+	Log.warn('Log', 'Enabled limited non ES6 support, only Log(v)(args), Log.warn(v)(args) and Log.error(v)(args) are available!');
 }
 
-var Log, Proxy = Proxy || null;
+if(typeof Dom !== 'undefined'){
+	LogHelp.DBG = (parseInt(Dom.location.query.get('DBG')) || 0);
+}
 
-if(Proxy) Log = new Proxy(ConsoleWrap('log'), { get(target, method){ return ConsoleWrap(method); } });
 else{
-  // limited non ES6 support
-  Log = ConsoleWrap('log');
-  Log.warn = ConsoleWrap('warn');
-  Log.error = ConsoleWrap('error');
+	module.exports = Log;
+
+	LogHelp.isNode = true;
+	LogHelp.mapColors = process.env.COLOR;
+	LogHelp.DBG = process.env.DBG;
 }
