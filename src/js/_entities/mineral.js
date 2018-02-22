96aba298f87eb6f58fb1ea037366cb6057ad12a0
fatlard1993 <fatlard1993@gmail.com>
2018-02-21 23:17:53 -0800

    cleanup and split out mineral collect logic

--- src/js/_entities/mineral.js
+++ src/js/_entities/mineral.js
@@ -9,7 +9,7 @@ Game.entities.mineral = function(x, y){
 Game.entities.mineral.prototype = Object.create(Phaser.Sprite.prototype);
 Game.entities.mineral.prototype.constructor = Game.entities.mineral;
 
-Game.entities.mineral.types = ['green', 'red', 'blue', 'purple', 'teal', '???'];
+Game.entities.mineral.types = ['green', 'red', 'blue', 'purple', 'teal', 'unknown'];
 
 Game.entities.mineral.create = function(x, y, type){
 	var mineral = Game.minerals.getFirstDead();
@@ -25,10 +25,32 @@ Game.entities.mineral.create = function(x, y, type){
 	mineral.frame = Game.entities.mineral.types.indexOf(type.replace('mineral_', ''));
 	mineral.type = type;
 
-	var gridPos = {
-		x: Game.toGridPos(x),
-		y: Game.toGridPos(y)
-	};
-
 	return mineral;
+};
+
+Game.entities.mineral.collect = function(pos){
+	var mineralWeight = 0.08;
+
+	if(Game.player.hull.space < mineralWeight) return;
+
+	Game.minerals.forEachAlive(function(mineral){
+		if(mineral.x === pos.x && mineral.y === pos.y){
+			Game.player.hull[mineral.type] = Game.player.hull[mineral.type] !== undefined ? Game.player.hull[mineral.type] : 0;
+
+			Game.player.hull[mineral.type]++;
+
+			var animationTime = 200 + Math.ceil(Game.phaser.math.distance(Game.phaser.camera.x, Game.phaser.camera.y, mineral.x, mineral.y));
+
+			Game.phaser.add.tween(mineral).to({ x: Game.phaser.camera.x, y: Game.phaser.camera.y }, animationTime, Phaser.Easing.Quadratic.Out, true);
+
+			setTimeout(function(){
+				Game.player.hull.space -= mineralWeight;
+
+				// Game.config.map[Game.toGridPos(mineral.x)][Game.toGridPos(mineral.y)][1] = -1;
+				// Game.config.viewBufferMap[Game.toGridPos(mineral.x)][Game.toGridPos(mineral.y)][1] = -1;
+
+				mineral.kill();
+			}, animationTime);
+		}
+	});
 };
